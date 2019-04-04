cd7610d2c29214754176f8d6994712307ad70fc3
jacopobertolini <48251328+jacopobertolini@users.noreply.github.com>
2019-04-04 10:31:31 +0200

    Square Implementation

--- src/main/java/it/polimi/ingsw/WeaponDeck.java
+++ src/main/java/it/polimi/ingsw/WeaponDeck.java
@@ -1,7 +1,26 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeaponDeck {
 
+    List<Card> cards;
+
     public WeaponDeck() {
+        cards= new ArrayList<>();
+      /*  for (int i = 0; i <= 20; i++) {
+            cards.add(new Card());
+        }
+      */
+    }
+
+    public Card  getcard (){
+        return cards.remove(0);
+    }
+    public void pushCard(Card card ){
+        cards.add(0, card);
     }
 }
+
+
