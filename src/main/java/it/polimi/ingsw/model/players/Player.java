ac11e82eda9573c0776d2103e3987eb04684866b
jacopobertolini <48251328+jacopobertolini@users.noreply.github.com>
2019-04-05 22:03:03 +0200

    updated Player.java

--- src/main/java/it/polimi/ingsw/model/players/Player.java
+++ src/main/java/it/polimi/ingsw/model/players/Player.java
@@ -2,6 +2,10 @@ package it.polimi.ingsw.model.players;
 
 import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.board.rooms.Square;
+import it.polimi.ingsw.model.bridges.ActionBridge;
+import it.polimi.ingsw.model.bridges.Bridge;
+import it.polimi.ingsw.model.bridges.DamageBridge;
+import it.polimi.ingsw.model.bridges.Shots;
 import it.polimi.ingsw.model.cards.Card;
 import it.polimi.ingsw.model.cards.CardHandler;
 import it.polimi.ingsw.model.cards.Target;
@@ -18,12 +22,47 @@ public class Player implements Target {
     private List<Card> weaponDeck;
     private Color playerColor;
     private Square currentPosition;
+    private int points;
+    private Bridge bridge = new Bridge();
+    private int tempPoints;
+
+    public void setBridge(Bridge bridge) {
+        this.bridge = bridge;
+    }
+
+    public Bridge getBridge() {
+        return bridge;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public void setPlayerColor(Color playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+
+    public CardHandler getCardHandler() {
+        return cardHandler;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
 
     public Player(String playerID, Color playerColor) {
 
         this.playerID = playerID;
         this.playerColor = playerColor;
         this.weaponDeck = new ArrayList<>();
+        this.points = 0;
+        this.tempPoints = 0;
     }
 
     public void setCurrentPosition(Square currentPosition) {
@@ -64,6 +103,7 @@ public class Player implements Target {
     }
 
     public void setDamage(Player enemy, int quantity) {
+        this.bridge.setDamage(this.getPlayerColor(), quantity);
         System.out.println("Damaged by " + enemy.getPlayerID());
         System.out.println("Enemy color " + enemy.getPlayerColor());
         System.out.println("Quantity " + quantity);
@@ -73,5 +113,25 @@ public class Player implements Target {
         System.out.println("Marked by " + enemy.getPlayerID());
         System.out.println("Enemy color " + enemy.getPlayerColor());
         System.out.println("Quantity " + quantity);
+    }
+
+    public int countPoints(DamageBridge damageBridge) {
+        List<Shots> shots = damageBridge.getShots();
+        for (Shots shots1 : shots) {
+            if (shots1.getColor().equals(this.getPlayerColor())) {
+                tempPoints++;
+            }
+        }
+        return tempPoints;
+
+
+    }
+
+    public void setTempPoints(int tempPoints) {
+        this.tempPoints = tempPoints;
+    }
+
+    public int getTempPoints() {
+        return tempPoints;
     }
 }
