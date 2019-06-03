a144e0f3912253f41baa0c097e5f9d265ce3c898
Amedeo Cavallo <amedeo.cavallo96@gmail.com>
2019-06-03 12:11:44 +0200

    Color fixes

--- src/main/java/it/polimi/ingsw/server/model/cards/effects/properties/PropertiesAnalyzer.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/effects/properties/PropertiesAnalyzer.java
@@ -141,12 +141,6 @@ public class PropertiesAnalyzer {
                         .computeDistance(activeSquare, target.getCurrentPosition(),
                                 effect.isCardinal(), effect.isThroughWalls());
 
-                System.out.println(effect.isCardinal());
-                System.out.println(effect.isThroughWalls());
-                System.out.println(ViewInspector
-                        .computeDistance(activeSquare, target.getCurrentPosition(),
-                                effect.isCardinal(), effect.isThroughWalls()));
-
                 // Launch exception if the distance is lower than the minDist property
                 if (effect.getMinDist() != null && distance < effect.getMinDist()) {
 
--- src/main/java/it/polimi/ingsw/server/model/cards/effects/properties/ViewInspector.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/effects/properties/ViewInspector.java
@@ -23,8 +23,6 @@ class ViewInspector {
     static int computeDistance(Square fromSquare, Square toSquare, boolean cardinal,
             boolean throughWalls) throws SquareDistanceException {
 
-        System.out.println("fromSquare " + fromSquare.getRoom().getColor() + " " + fromSquare.getSquareId());
-        System.out.println("toSquare " + toSquare.getRoom().getColor() + " " + toSquare.getSquareId());
         // Return compute cardinal distance if cardinal true
         if (cardinal) {
 
