692decca9bfe8b33155ba8afc4b3af7cfde701a9
federicoCapaccio <federico.capaccio@mail.polimi.it>
2019-07-05 11:39:19 +0200

    Javadoc

--- src/main/java/it/polimi/ingsw/server/model/cards/effects/Effect.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/effects/Effect.java
@@ -11,6 +11,11 @@ import java.util.stream.Collectors;
 import javax.json.Json;
 import javax.json.JsonObject;
 
+/**
+ * This class represents the effect of a Card or an action. An effect can be Primary, Alternative,
+ * Optional1 and Optional2. This class has a list of flags needed to allow the EffectHandler to
+ * check if a certain effect can be executed or not. It is the core of the logic of properties.
+ */
 public class Effect implements Serializable {
 
     /**
@@ -161,111 +166,222 @@ public class Effect implements Serializable {
         this.atomicEffectList = builder.atomicEffectList;
     }
 
+    /**
+     * Gets the id of the effect.
+     *
+     * @return The id of the effect.
+     */
     public int getId() {
 
         return this.id;
     }
 
+    /**
+     * Gets the args of the effect.
+     *
+     * @return The args of the effect.
+     */
     public double getArgs() {
 
         return this.args;
     }
 
+
+    /**
+     * Gets the name of the effect.
+     *
+     * @return The name of the effect.
+     */
     public String getName() {
 
         return this.name;
     }
 
+    /**
+     * Gets the name of the effect.
+     *
+     * @return The name of the effect.
+     */
     public String getDescription() {
 
         return this.description;
     }
 
+    /**
+     * Gets the name of the effect.
+     *
+     * @return The name of the effect.
+     */
     public TargetType getTargetType() {
 
         return this.targetType;
     }
 
+    /**
+     * Checks if the effect is used.
+     *
+     * @return The used property.
+     */
     public boolean isUsed() {
 
         return this.used;
     }
 
+    /**
+     * Sets the used property.
+     *
+     * @param used The used boolean value.
+     */
     public void setUsed(boolean used) {
 
         this.used = used;
     }
 
+    /**
+     * Gets the next effect of the effect.
+     *
+     * @return The next effect of the effect.
+     */
     public Effect getNext() {
 
         return this.next;
     }
 
+    /**
+     * Gets the list of id of optional effects of the effect.
+     *
+     * @return The list of integer.
+     */
     public List<Integer> getOptionalId() {
 
         return this.optionalId;
     }
 
+    /**
+     * Gets the cost of the effect.
+     *
+     * @return The list of color - the cost of the effect.
+     */
     public List<Color> getCost() {
 
         return this.cost;
     }
 
+    /**
+     * Gets the activated property of the effect.
+     *
+     * @return The activated property of the effect.
+     */
     public Boolean getActivated() {
 
         return this.activated;
     }
 
+    /**
+     * Sets the activated property.
+     *
+     * @param activated The activated value.
+     */
     public void setActivated(Boolean activated) {
 
         this.activated = activated;
     }
 
+    /**
+     * Gets the maxArgs property of the effect.
+     *
+     * @return The maxArgs property of the effect.
+     */
     public Integer getMaxTargets() {
 
         return this.maxTargets;
     }
 
+    /**
+     * Gets the sameAsFather property of the effect.
+     *
+     * @return The list of Boolean - the sameAsFather property of the effect.
+     */
     public List<Boolean> getSameAsFather() {
 
         return this.sameAsFather;
     }
 
+    /**
+     * Checks the sameAsPlayer property.
+     *
+     * @return The boolean of the sameAsPlayer property.
+     */
     public boolean isSameAsPlayer() {
 
         return this.sameAsPlayer;
     }
 
+    /**
+     * Gets the targetView property of the effect.
+     *
+     * @return The targetView property of the effect.
+     */
     public Boolean getTargetView() {
 
         return this.targetView;
     }
 
+    /**
+     * Checks the seenByActive property.
+     *
+     * @return The boolean of the seenByActive property.
+     */
     public boolean isSeenByActive() {
 
         return this.seenByActive;
     }
 
+    /**
+     * Gets the minDist property of the effect.
+     *
+     * @return The minDist property of the effect.
+     */
     public Integer getMinDist() {
 
         return this.minDist;
     }
 
+    /**
+     * Gets the maxDist property of the effect.
+     *
+     * @return The maxDist property of the effect.
+     */
     public Integer getMaxDist() {
 
         return this.maxDist;
     }
 
+    /**
+     * Checks the cardinal property.
+     *
+     * @return The boolean of the cardinal property.
+     */
     public boolean isCardinal() {
 
         return this.cardinal;
     }
 
+    /**
+     * Checks the throughWalls property.
+     *
+     * @return The boolean of the throughWalls property.
+     */
     public boolean isThroughWalls() {
 
         return this.throughWalls;
     }
 
+    /**
+     * Checks the differentSquares property.
+     *
+     * @return The boolean of the differentSquares property.
+     */
     public boolean isDifferentSquares() {
 
         return this.differentSquares;
--- src/main/java/it/polimi/ingsw/server/model/cards/effects/EffectArgument.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/effects/EffectArgument.java
@@ -6,70 +6,133 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class hold all the information needed to execute an effect, in every possible case.
+ */
 public class EffectArgument implements Serializable {
 
+    /**
+     * The destination of the Target in the targetList.
+     */
     private Square destination;
+
+    /**
+     * The list of Target. It can be a list of Square, a list with one or more Player or a list with
+     * one Room.
+     */
     private List<Target> targetList;
 
+    /**
+     * A boolean that distinguishes weaponCards effect by effects of actions or power ups.
+     */
     private boolean weaponCard = true;
 
+    /**
+     * Creates the EffectArgument.
+     */
     public EffectArgument() {
 
         this.targetList = new ArrayList<>();
     }
 
+    /**
+     * Creates the EffectArgument with the destination Square.
+     *
+     * @param destination The square destination.
+     */
     public EffectArgument(Square destination) {
 
         this.destination = destination;
         this.targetList = new ArrayList<>();
     }
 
+    /**
+     * Creates the EffectArgument with the list of Target.
+     * @param targetList The list of Target.
+     */
     public EffectArgument(List<Target> targetList) {
 
         this.targetList = targetList;
     }
 
+    /**
+     * Creates the EffectArgument with the destination Square and the list of Target.
+     * @param destination  The destination Square.
+     * @param targetList The list of Target.
+     */
     public EffectArgument(Square destination, List<Target> targetList) {
 
         this.destination = destination;
         this.targetList = targetList;
     }
 
+    /**
+     * Gets the destination.
+     * @return The Square destination.
+     */
     public Square getDestination() {
 
         return this.destination;
     }
 
+    /**
+     * Sets the destination.
+     * @param destination The destination.
+     */
     public void setDestination(Square destination) {
 
         this.destination = destination;
     }
 
+    /**
+     * Gets the targetList.
+     * @return The list of Target.
+     */
     public List<Target> getTargetList() {
 
         return this.targetList;
     }
 
+    /**
+     * Sets the targetList.
+     * @param targetList The List of Target.
+     */
     public void setTargetList(List<Target> targetList) {
 
         this.targetList = targetList;
     }
 
+    /**
+     * Adds a target to the target list.
+     * @param target The Target to add.
+     */
     public void appendTarget(Target target) {
 
         this.targetList.add(target);
     }
 
+    /**
+     * Checks if is weaponCard.
+     * @return The boolean value.
+     */
     public boolean isWeaponCard() {
 
         return this.weaponCard;
     }
 
+    /**
+     * Sets the weaponCard value.
+     * @param weaponCard The boolean value.
+     */
     public void setWeaponCard(boolean weaponCard) {
 
         this.weaponCard = weaponCard;
     }
 
+    /**
+     * Gets the args needed for this EffectArgument.
+     * @return The double value.
+     */
     public double getArgs() {
 
         if (this.destination == null && this.targetList.isEmpty()) {
